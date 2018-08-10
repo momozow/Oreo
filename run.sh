14d3fd4e0c6e10d95878945e2028df78eb55d31b
momozow <global.n@me.com>
2018-08-10 13:33:41 +0000

    run go

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker run --rm --name oreo -v /Users/momozow/.ssh:/root/.ssh:ro -v /Users/momozow/.gitconfig:/root/.gitconfig:ro -it oreo /usr/bin/fish
+docker run \
+       --rm \
+       --name oreo.Go \
+       -v /Users/momozow/.ssh:/root/.ssh:ro \
+       -v /Users/momozow/.gitconfig:/root/.gitconfig:ro \
+       -it oreo:Go /usr/bin/fish
